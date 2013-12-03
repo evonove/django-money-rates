e8159c0c512163cca0b4487239dab33960ee70ee
Tim Zenderman <tzenderman@gmail.com>
2013-12-03 15:56:37 -0300

    Refactor a small bit of the utils funcs.

--- djmoney_rates/utils.py
+++ djmoney_rates/utils.py
@@ -11,36 +11,42 @@ def convert_money(amount, currency_from, currency_to):
     """
     Convert 'amount' from 'currency_from' to 'currency_to'
     """
-    backend = money_rates_settings.DEFAULT_BACKEND()
 
+def get_rate(currency):
+    """Returns the rate from the default currency to `currency`."""
+    source = get_rate_source()
+    try:
+        return Rate.objects.get(source=source, currency=currency).value
+    except Rate.DoesNotExist:
+        raise CurrencyConversionException(
+            "Rate for %s in %s do not exists. "
+            "Please run python manage.py update_rates" % (
+                currency, source.name))
+
+
+def get_rate_source():
+    """Get the default Rate Source and return it."""
+    backend = money_rates_settings.DEFAULT_BACKEND()
     try:
         source = RateSource.objects.get(name=backend.get_source_name())
+        return RateSource.objects.get(name=backend.get_source_name())
     except RateSource.DoesNotExist:
         raise CurrencyConversionException(
             "Rate for %s source do not exists. "
             "Please run python manage.py update_rates" % backend.get_source_name())
 
     # get rate for currency_from
+    source = get_rate_source()
+
+    # Get rate for currency_from.
     if source.base_currency != currency_from:
-        try:
-            rate_from = Rate.objects.get(source=source, currency=currency_from).value
-        except Rate.DoesNotExist:
-            raise CurrencyConversionException(
-                "Rate for %s in %s do not exists. "
-                "Please run python manage.py update_rates" % (
-                    currency_from, source.name))
+        rate_from = get_rate(currency_from)
     else:
-        # If currency from is the same as base currency its rate is 1
+        # If currency from is the same as base currency its rate is 1.
         rate_from = Decimal(1)
 
-    # get rate for currency_to
-    try:
-        rate_to = Rate.objects.get(source=source, currency=currency_to).value
-    except Rate.DoesNotExist:
-        raise CurrencyConversionException(
-            "Rate for %s in %s do not exists. "
-            "Please run python manage.py update_rates" % (
-                currency_to, source.name))
+    # Get rate for currency_to.
+    rate_to = get_rate(currency_to)
 
     if isinstance(amount, float):
         amount = Decimal(amount).quantize(Decimal('.000001'))
